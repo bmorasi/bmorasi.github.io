95135f13b3426819127502e2bdef9dd195e648f8
bmorasi <best.morasi@live.nl>
2025-03-22 12:28:46 +0100

    Grammar/spelling corrections

--- src/data/translations.ts
+++ src/data/translations.ts
@@ -81,23 +81,23 @@ export const translations: TranslationData = {
       ]]
     ],
     nl: [
-      ["Klantcontract medewerker KYC @ NN Bank", "Juni 2024 - heden", [
-        " Als klantcontact medewerker ondersteun ik samen met mijn collega's klanten bij het identificeren van persoonsgegevens door digitale begeleiding te bieden via een technische tooling waarbij het beheersen van de gehanteerde techniek en het inzicht hebben waar klanten technisch over struikelen van belang is. Dit doe ik met een focus op klantvriendelijkheid en nauwkeurigheid. Daarbij draag ik actief bij aan de verbetering van systemen door issues te signaleren en door te geven.",
+      ["Klantcontact medewerker KYC @ NN Bank", "Juni 2024 - heden", [
+        "Als klantcontact medewerker ondersteun ik samen met mijn collega's klanten bij het identificeren van persoonsgegevens door digitale begeleiding te bieden via een technische tooling waarbij het beheersen van de gehanteerde techniek en het inzicht hebben waar klanten technisch over struikelen van belang is. Dit doe ik met een focus op klantvriendelijkheid en nauwkeurigheid. Daarbij draag ik actief bij aan de verbetering van systemen door issues te signaleren en door te geven.",
         "- Soft skills: Oplossingsgericht, geduld, klantgerichtheid, communicatie",
         "- Hard skills: JavaScript, HTML & CSS, TypeScript, Vue.JS, Git, Salesforce"
       ]],
       ["Software Engineer Stagiair @ Air Innovations", "Oktober 2023 - April 2024", [
-        "Als stagair Software Engineer heb ik zelfstanding in een Agile omgeving gewerkt aan een prototype feature van een full-stack applicatie waarmee gebruikers in een 3D-omgeving een vliegtuigmotor kunnen verkennen. De applicatie bevat interactieve functionaliteiten, zoals het opvragen van informatie over specifieke motoronderdelen.",
+        "Als stagiair Software Engineer heb ik zelfstandig in een Agile omgeving gewerkt aan een prototype feature van een full-stack applicatie waarmee gebruikers in een 3D-omgeving een vliegtuigmotor kunnen verkennen. De applicatie bevat interactieve functionaliteiten, zoals het opvragen van informatie over specifieke motoronderdelen.",
         "- Soft skills: Stressbestendig, kritisch denken, gericht research, zelfstandig werken",
         "- Hard skills: JavaScript, HTML & CSS, Three.JS, Vue.JS, Git"
       ]],
       ["Junior Software Engineer @ Air Innovations", "Juni 2023 - Oktober 2023", [
-        " Als Junior Software Engineer heb ik in een Agile omgeving gewerkt aan het onderhouden van een full-stack webapplicatie dat gebruikt wordt voor het toevoegen van kunstmatige intelligentie aan het onderhouden van vliegtuig motoren. In de font-end gebruik gemaakt van JavaScript, HTML & CSS, waarbij er aandacht besteed wordt aan de gebruiksvriendelijkheid voor de eindgebruikers. In de back-end gebruik gemaakt van Python.",
+        "Als Junior Software Engineer heb ik in een Agile omgeving gewerkt aan het onderhouden van een full-stack webapplicatie die gebruikt wordt voor het toevoegen van kunstmatige intelligentie aan het onderhouden van vliegtuigmotoren. In de front-end gebruik gemaakt van JavaScript, HTML & CSS, waarbij er aandacht besteed wordt aan de gebruiksvriendelijkheid voor de eindgebruikers. In de back-end gebruik gemaakt van Python.",
         "- Soft skills: Kritisch communiceren, stressbestendig, kritisch denken, SCRUM",
         "- Hard skills: Python, JavaScript, HTML & CSS, Vue.JS, Git"
       ]],
-      ["Software Engineer stagiaire @ Air Innovations", "2022 - 2022", [
-        "Als stagair Software Engineer heb ik in een Agile omgeving gewerkt aan een nieuwe functie voor een bestaand product dat kunstmatige intelligentie toepast op het onderhoud van vliegtuigmotoren. Voor de front-end is JavaScript, HTML en CSS gebruikt, met focus op gebruiksvriendelijkheid voor de eindgebruikers. De back-end is ontwikkeld in Python en GStreamer, en de front- en back-end zijn gekoppeld met REDIS. Voor continuous development en versiebeheer heb ik gebruik gemaakt van GIT.",
+      ["Software Engineer stagiair @ Air Innovations", "2022 - 2022", [
+        "Als stagiair Software Engineer heb ik in een Agile omgeving gewerkt aan een nieuwe functie voor een bestaand product dat kunstmatige intelligentie toepast op het onderhoud van vliegtuigmotoren. Voor de front-end is JavaScript, HTML en CSS gebruikt, met focus op gebruiksvriendelijkheid voor de eindgebruikers. De back-end is ontwikkeld in Python en GStreamer, en de front- en back-end zijn gekoppeld met REDIS. Voor continuous development en versiebeheer heb ik gebruik gemaakt van GIT.",
         "- Soft skills: oplossingsgericht denken, creatief denken",
         "- Hard skills: Python, JavaScript, HTML & CSS, Vue.JS, Git"
       ]]
@@ -168,13 +168,13 @@ export const translations: TranslationData = {
       ]]
     ],
     nl: [
-      ["Estanged Manor", "2022", [
-        " In groepsverband een video-game gemaakt in de genre van 3D horror/thriller platformer op PC. Hierbij gebruik gemaakt van Unity, en in een groep van 8 gewerkt in een Agile omgeving met gebruik van SCRUM.",
+      ["Estranged Manor", "2022", [
+        "In groepsverband een video-game gemaakt in het genre van 3D horror/thriller platformer op PC. Hierbij gebruik gemaakt van Unity, en in een groep van 8 gewerkt in een Agile omgeving met gebruik van SCRUM.",
         "- Soft skills: teamwerk, effectieve communicatie",
         "- Hard skills: C#, SCRUM"
       ]],
       ["Buurt Applicatie", "2020-2021", [
-        " In een kleine groep gewerkt met het maken van een webapplicatie, waarbij buurtbewoners met elkaar kunnen communiceren en ervaringen uit de buurt kunnen delen. Gebruik gemaakt van .NET voor de backend. In de frontend is er gebruik gemaakt van Bootstrap, HTML & CSS, waarbij er rekening gehouden is met de gebruiksvriendelijkheid van de website. ",
+        "In een kleine groep gewerkt met het maken van een webapplicatie, waarbij buurtbewoners met elkaar kunnen communiceren en ervaringen uit de buurt kunnen delen. Gebruik gemaakt van .NET voor de backend. In de frontend is er gebruik gemaakt van Bootstrap, HTML & CSS, waarbij er rekening gehouden is met de gebruiksvriendelijkheid van de website.",
         "- Soft skills: teamwerk, effectieve communicatie",
         "- Hard skills: .NET frameworks, Bootstrap"
       ]],
